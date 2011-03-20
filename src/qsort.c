f5b39648e906fa4821fe97c16fcac56a5fd583fd
Barry Friedman <barryf@google.com>
2011-03-19 21:54:47 -0700

    Initial checkin of Qsort
    * AUTHORS: Author list
    * COPYING: License info
    * ChangeLog: Empty (will reference public log once available)
    * INSTALL: Build and installation instructions
    * Makefile.am: Build input file
    * NEWS: Empty (will add info upon release)
    * README: Package description
    * configure.ac: Build configuration file
    * src/Makefile.am: Build input file
    * src/qsort.c: (main, quicksort, usage, version) Functions to
      demonstrate quicksort algorithm

---
+++ src/qsort.c
@@ -0,0 +1,192 @@
+/*
+ * qsort -- test and measure timing for the qsort algorithm
+ * Copyright (C) 2011  Barry Friedman (friedman.barry@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/time.h>
+#include <config.h>
+
+/*
+ * Swap contents of arguments a and b
+ */
+static inline void
+swap (int *a, int *b)
+{
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+/*
+ * Sort integer array of length len using Quicksort algorithm
+ */
+static void
+quicksort (int *array, int len)
+{
+  if (len <= 1)
+    return;
+  int pidx = rand () % len;
+  swap (&array[0], &array[pidx]);
+  int i,last=0;
+  for (i=1; i<len; ++i)
+    {
+      if (array[i] < array[0])
+        swap (&array[i], &array[++last]);
+    }
+  swap (&array[0], &array[last]);
+  quicksort (&array[0], last);
+  quicksort (&array[(last+1)], len-(last+1));
+}
+
+/*
+ * Display version information
+ */
+static void
+version (void)
+{
+  printf ("%s\n"
+          "Copyright (C) 2011 Barry Friedman\n"
+          "License GPLv3+: GNU GPL version 3 or later "
+          "<http://gnu.org/licenses/gpl.html>\n"
+          "This is free software: You are free to change and redistribute it.\n"
+          "This program comes with ABSOLUTELY NO WARRANTY.\n",
+          PACKAGE_STRING);
+  exit(0);
+}
+
+/*
+ * Display usage information
+ */
+static void
+usage (int errcode)
+{
+  fprintf (stderr,
+           "Usage: qsort --count=<count> [optargs]\n"
+           "       <count> is the number of random integers to sort\n"
+           "       optargs:\n"
+           "       --print     print out sorted array\n"
+           "       --timing    print sort time in microseconds\n"
+           "       --help      print usage information\n"
+           "       --version   output version information and exit\n"
+           "Report bugs to: %s\n"
+           "%s home page: %s\n",
+           PACKAGE_BUGREPORT, PACKAGE_NAME, PACKAGE_URL);
+  exit(errcode);
+}
+
+/*
+ * Program options array
+ */
+static struct option long_options[] = {
+  {"count",    required_argument,  0,   'c'},
+  {"print",    no_argument,        0,   'p'},
+  {"timing",   no_argument,        0,   't'},
+  {"help",     no_argument,        0,   'h'},
+  {"version",  no_argument,        0,   'v'},
+  {0, 0, 0, 0}
+};
+
+/*
+ * Program main
+ *
+ * Parse arguments, generate an array of random integers to sort,
+ * then sort array while measuring timing. Optionally display
+ * sort time and/or sorted array.
+ */
+int
+main (int argc, char **argv)
+{
+  int vsize, print, timing;
+
+  print = timing = 0;
+  vsize = 0;
+
+  while (1)
+    {
+      int c, option_index = 0;
+      c = getopt_long (argc, argv, "c:hpt", long_options, &option_index);
+      if (c == -1)
+        break;
+      switch (c)
+        {
+        case 'c':
+          vsize = atoi (optarg);
+          break;
+        case 'p':
+          print = 1;
+          break;
+        case 't':
+          timing = 1;
+          break;
+        case 'h':
+          usage(0);
+          break;
+        case 'v':
+          version();
+          break;
+        case '?':
+          break;
+        default:
+          abort();
+        }
+    }
+  
+  if (!vsize)
+    {
+      fprintf (stderr, "required count not specified!\n");
+      usage (1);
+    }
+
+  int *testvec = malloc (sizeof(int) * vsize);
+  if (!testvec)
+    {
+      fprintf (stderr, "failed to allocate memory for %d integers\n", vsize);
+      exit (1);
+    }
+
+  int i;
+
+  for (i=0; i<vsize; i++)
+    testvec[i] = rand () % 1000;
+
+  struct timeval before, after;
+  
+  gettimeofday (&before, NULL);
+  quicksort (testvec, vsize);
+  gettimeofday (&after, NULL);
+
+  if (print)
+    {
+      printf ("{");
+      for (i = 0; i<vsize; i++)
+        printf ("%d%s", testvec[i], i==(vsize-1) ? "}\n" : ", ");
+    }
+
+  if (timing)
+    {
+      unsigned long diffsec = after.tv_sec - before.tv_sec;
+      unsigned long diffusec = after.tv_usec - before.tv_usec;
+      unsigned long usec = diffsec * 1000000;
+      usec += diffusec;
+      printf ("%lu\n", usec);
+    }
+
+  free (testvec);
+  exit (0);
+}
